6a455984334b705342e8195cb412a80394599a4a
ilahi990 <fahadilahi990@gmail.com>
2024-10-01 06:26:23 -0700

    checking the work flow

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,12 @@
-hsljkh
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+      <h1>cheking the work flow</h1>
+</body>
+</html>
